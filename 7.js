f73743de5658c8d15e0543814ef3d90ead589073
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-18 13:31:29 +0300

    7

--- 7.js
+++ 7.js
@@ -3,20 +3,14 @@ let getResult = (data) => {
 	let parse_ints = str => {
 		return str.split(' ').filter(val => val != '').map(val => parseInt(val.trim()));
 	}
-	let map = new Map();
 	let count = parseInt(data[0]);
-	for(let i = 1; i <= count; i++) {
-		map.set(i, 0);
-	}
+	
 	let nulled = [];
 	for(let i = 1; i <= count; i++) {
 		let arr = parse_ints(data[i]);		
 		if (arr[0] === 0) {
 			nulled.push(i);
-		}
-		for (let j = 1; j <= arr[0]; j++){
-			map.set(arr[j], (map.get(arr[j]) ?? 0) + 1);
-		}
+		}		
 	}			
 
 	return nulled.reduce((acc, val) => acc+=val);

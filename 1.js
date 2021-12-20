c39f78b3017f4c0ff614663cc4af6bdc9f6e03be
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-20 13:24:44 +0300

    1

---
+++ 1.js
@@ -0,0 +1,22 @@
+let getResult = (data) => {
+
+	let parse_ints = str => {
+		return str.split(' ').filter(val => val != '').map(val => parseInt(val.trim()));
+	}
+
+	let [up, down] = parse_ints(data[0]);
+	
+
+	return up + down + Math.min(up, down);
+
+}
+//console.log('start: ' + Date.now());
+const fs = require('fs');
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const data = fileContent.toString().trim().split("\n");
+
+const result = getResult(data);
+
+fs.writeFileSync("output.txt", result.toString());
+//console.log('end: ' + Date.now());

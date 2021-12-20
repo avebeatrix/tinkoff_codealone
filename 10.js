c5e8a2902333727e43129de933357d01ec7a8c54
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-20 13:21:44 +0300

    10

---
+++ 10.js
@@ -0,0 +1,48 @@
+let getResult = (data) => {
+
+	let parse_ints = str => {
+		return str.split(' ').filter(val => val != '').map(val => parseInt(val.trim()));
+	}
+
+	let n = parseInt(data[0]);
+	let arr = parse_ints(data[1]);	
+	
+	let checkHorizontal = () => {
+		let cur;
+		let max = 0;
+		for(let k = 1; k < 10000; k++) {
+			for(let i = 0; i < n - 1; i++) {
+				if (arr[i]>=k){
+					cur = k;
+					for(let j = i + 1; j < n; j++) {
+						if (arr[j] >= k) {
+							cur += k;
+						} else {							
+							break;
+						}			
+					}
+					max = Math.max(max, cur);	
+				}				
+			}
+		}
+		return max;
+	}		
+	let max = checkHorizontal();
+	arr.reverse();	
+
+	max = Math.max(max, checkHorizontal());
+	arr.reverse();	
+		
+	return max;
+}
+const { count } = require('console');
+//console.log('start: ' + Date.now());
+const fs = require('fs');
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const data = fileContent.toString().trim().split("\n");
+
+const result = getResult(data);
+
+fs.writeFileSync("output.txt", result.toString());
+//console.log('end: ' + Date.now());
